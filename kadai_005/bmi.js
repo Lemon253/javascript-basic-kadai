19053429f53d43ec864d81dde427076ff2fdbe96
Lemon <104106769+Lemon253@users.noreply.github.com>
2025-02-16 08:42:43 +0900

    課題「四則演算をしよう」提出

---
+++ kadai_005/bmi.js
@@ -0,0 +1,10 @@
+let weight = 68;  //体重
+let height = 1.7; //身長
+
+//確認用ログ
+//console.log("weight = "+weight," height = "+height);
+
+const bmi = weight/height*height;
+
+console.log("BMI = "+bmi);
+
