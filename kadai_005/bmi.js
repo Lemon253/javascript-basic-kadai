e0550556451d4e6e67e4202456e76e5eed87b580
Lemon <104106769+Lemon253@users.noreply.github.com>
2025-02-16 09:01:11 +0900

    計算式の部分を修正

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -4,7 +4,7 @@ let height = 1.7; //身長
 //確認用ログ
 //console.log("weight = "+weight," height = "+height);
 
-const bmi = weight/height*height;
+const bmi = weight/(height*height);
 
 console.log("BMI = "+bmi);
 
